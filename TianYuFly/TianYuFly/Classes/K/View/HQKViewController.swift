c952ca891a6beedc9cb7b5c8c744bece6d314114
hongqingWang <272338444@qq.com>
2017-08-07 11:01:05 +0800

    准备布局(C)消息列表页

--- TianYuFly/TianYuFly/Classes/K/View/HQKViewController.swift
+++ TianYuFly/TianYuFly/Classes/K/View/HQKViewController.swift
@@ -13,23 +13,16 @@ class HQKViewController: HQBaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+extension HQKViewController {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func setupTableView() {
+        super.setupTableView()
+        
+        view = UIView(frame: CGRect(x: 0, y: 64, width: UIScreen.hq_screenWidth(), height: 100))
+        view.backgroundColor = UIColor.red
     }
-    */
-
 }
