9845a38d099180e7f2df9776005d690ba77ba077
hongqingWang <272338444@qq.com>
2017-08-07 17:31:34 +0800

    添加(C)消息详情模块控制器和Cell，准备布局

---
+++ TianYuFly/TianYuFly/Classes/C/View/HQCDetailCell.swift
@@ -0,0 +1,24 @@
+//
+//  HQCDetailCell.swift
+//  TianYuFly
+//
+//  Created by 王红庆 on 2017/8/7.
+//  Copyright © 2017年 王红庆. All rights reserved.
+//
+
+import UIKit
+
+class HQCDetailCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
