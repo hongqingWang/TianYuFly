3b7bba1dbb905fd105c66ed575cf21c6db4df767
hongqingWang <272338444@qq.com>
2017-08-13 22:16:40 +0800

    添加HQDCell

---
+++ TianYuFly/TianYuFly/Classes/D/View/HQDCell.swift
@@ -0,0 +1,24 @@
+//
+//  HQDCell.swift
+//  TianYuFly
+//
+//  Created by 王红庆 on 2017/8/13.
+//  Copyright © 2017年 王红庆. All rights reserved.
+//
+
+import UIKit
+
+class HQDCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- TianYuFly/TianYuFly/Classes/D/View/HQDViewController.swift
+++ TianYuFly/TianYuFly/Classes/D/View/HQDViewController.swift
@@ -8,28 +8,20 @@
 
 import UIKit
 
+fileprivate let HQDCellId = "HQDCellId"
+
 class HQDViewController: HQBaseViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+// MARK: - UI
+extension HQDViewController {
+    
+    override func setupTableView() {
+        super.setupTableView()
+        
+        tableView?.register(HQDCell.classForCoder(), forCellReuseIdentifier: HQDCellId)
     }
-    */
-
 }
